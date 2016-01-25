bbc44ac35f8634d7653ac29e1e23053e992cda15
Hong, Dewey <doohee323@gmail.com>
2016-01-24 22:42:21 -0800

    map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,6 @@
+/////////////////////////////////////////////////////////////////////////////////
+// [ line chart ]
+/////////////////////////////////////////////////////////////////////////////////
 var main_margin = {
   top : 20,
   right : 80,
@@ -252,6 +255,8 @@ function brush() {
   main.select(".line1").attr("d", main_line1);
   main.select(".line2").attr("d", main_line2);
   main.select(".x.axis").call(main_xAxis);
+
+  redraw(2000);
 }
 
 (function() {
@@ -366,3 +371,75 @@ function brush() {
 // return height - y(d.value);
 // });
 // });
+
+// ///////////////////////////////////////////////////////////////////////////////
+// [ map chart ]
+// ///////////////////////////////////////////////////////////////////////////////
+
+var w = 1200;
+var h = 500;
+
+var xy = d3.geo.equirectangular().scale(150);
+var path = d3.geo.path().projection(xy);
+
+var svg = d3.select("#graph").insert("svg:svg").attr("width", w).attr("height",
+    h);
+var states = svg.append("svg:g").attr("id", "states");
+var circles = svg.append("svg:g").attr("id", "circles");
+var labels = svg.append("svg:g").attr("id", "labels");
+
+d3.json("countries.json", function(collection) {
+  states.selectAll("path").data(collection.features).enter().append("svg:path")
+      .attr("d", path).on(
+          "mouseover",
+          function(d) {
+            d3.select(this).style("fill", "#6C0").append("svg:title").text(
+                d.properties.name);
+          }).on("mouseout", function(d) {
+        d3.select(this).style("fill", "#ccc");
+      })
+});
+
+// http://stackoverflow.com/questions/11386150/lat-lon-positon-on-a-d3-js-map
+// +convert to string to number
+
+var scalefactor = 1. / 50.;
+d3.csv("map.csv", function(csv) {
+  circles.selectAll("circle").data(csv).enter().append("svg:circle").attr("cx",
+      function(d, i) {
+        return xy([ +d["longitude"], +d["latitude"] ])[0];
+      }).attr("cy", function(d, i) {
+    return xy([ +d["longitude"], +d["latitude"] ])[1];
+  }).attr("r", function(d) {
+    return (+d["1990"]) * scalefactor;
+  }).attr("title", function(d) {
+    return d["country"] + ": " + Math.round(d["1990"]);
+  }).on("mouseover", function(d) {
+    d3.select(this).style("fill", "#FC0");
+  }).on("mouseout", function(d) {
+    d3.select(this).style("fill", "steelblue");
+  });
+
+  labels.selectAll("labels").data(csv).enter().append("svg:text").attr("x",
+      function(d, i) {
+        return xy([ +d["longitude"], +d["latitude"] ])[0];
+      }).attr("y", function(d, i) {
+    return xy([ +d["longitude"], +d["latitude"] ])[1];
+  }).attr("dy", "0.3em").attr("text-anchor", "middle").text(function(d) {
+    return Math.round(d["1990"]);
+  });
+});
+
+function redraw(year) {
+  circles.selectAll("circle").transition().duration(1000).ease("linear").attr(
+      "r", function(d) {
+        return (+d[year]) * scalefactor;
+      }).attr("title", function(d) {
+    return d["country"] + ": " + Math.round(d[year]);
+  });
+
+  labels.selectAll("text").text(function(d) {
+    return Math.round(d[year]);
+  });
+}
+
