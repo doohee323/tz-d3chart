cd61e0de726e7ff14802795efff41447b528775a
dewey hong <doohee323@gmail.com>
2016-01-28 09:41:07 -0800

    .

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -48,67 +48,6 @@ var UptimeChart = function(chartElem, mapElem, config) {
   this.mapData = new Array();
   this.scale = 1. / config.map.scale;
 
-  d3.legend = function(g) {
-    g.each(function() {
-      var g = d3.select(this), items = {};
-      this.svg = d3.select(chartElem);
-      var legendPadding = g.attr("data-style-padding") || 10;
-      var lb = g.selectAll(".legend-box").data([ true ]);
-      var li = g.selectAll(".legend-items").data([ true ]);
-
-      lb.enter().append("rect").classed("legend-box", true);
-      li.enter().append("g").classed("legend-items", true);
-
-      this.svg.selectAll("[data-legend]").each(
-          function() {
-            var self = d3.select(this);
-            items[self.attr("data-legend")] = {
-              pos : self.attr("data-legend-pos") || this.getBBox().y,
-              color : self.attr("data-legend-color") != undefined ? self
-                  .attr("data-legend-color")
-                  : self.style("fill") != 'none' ? self.style("fill") : self
-                      .style("stroke")
-            }
-          })
-
-      items = d3.entries(items).sort(function(a, b) {
-        return a.value.pos - b.value.pos;
-      })
-
-      li.selectAll("text").data(items, function(d) {
-        return d.key;
-      }).call(function(d) {
-        d.enter().append("text");
-      }).call(function(d) {
-        d.exit().remove();
-      }).attr("y", function(d, i) {
-        return i + "em";
-      }).attr("x", "1em").text(function(d) {
-        return d.key;
-      })
-
-      li.selectAll("circle").data(items, function(d) {
-        return d.key;
-      }).call(function(d) {
-        d.enter().append("circle");
-      }).call(function(d) {
-        d.exit().remove();
-      }).attr("cy", function(d, i) {
-        return i - 0.25 + "em";
-      }).attr("cx", 0).attr("r", "0.4em").style("fill", function(d) {
-        console.log(d.value.color);
-        return d.value.color;
-      })
-
-      var lbbox = li[0][0].getBBox();
-      lb.attr("x", (lbbox.x - legendPadding)).attr("y",
-          (lbbox.y - legendPadding)).attr("height",
-          (lbbox.height + 2 * legendPadding)).attr("width",
-          (lbbox.width + 2 * legendPadding));
-    })
-    return g;
-  }
-
   this.getSize = function(d, city) {
     var size = Math.round(this.getTotal(d, city) * this.scale);
     if (size >= 10) {
@@ -174,6 +113,67 @@ UptimeChart.prototype.makeUptimeChart = function(data) {
   var mini_xAxis = d3.svg.axis().scale(this.mini_x).tickFormat(
       d3.time.format("%H:%M")).orient("bottom");
 
+  d3.legend = function(g) {
+    g.each(function() {
+      var g = d3.select(this), items = {};
+      this.svg = d3.select(_self.chartElem);
+      var legendPadding = g.attr("data-style-padding") || 10;
+      var lb = g.selectAll(".legend-box").data([ true ]);
+      var li = g.selectAll(".legend-items").data([ true ]);
+
+      lb.enter().append("rect").classed("legend-box", true);
+      li.enter().append("g").classed("legend-items", true);
+
+      this.svg.selectAll("[data-legend]").each(
+          function() {
+            var self = d3.select(this);
+            items[self.attr("data-legend")] = {
+              pos : self.attr("data-legend-pos") || this.getBBox().y,
+              color : self.attr("data-legend-color") != undefined ? self
+                  .attr("data-legend-color")
+                  : self.style("fill") != 'none' ? self.style("fill") : self
+                      .style("stroke")
+            }
+          })
+
+      items = d3.entries(items).sort(function(a, b) {
+        return a.value.pos - b.value.pos;
+      })
+
+      li.selectAll("text").data(items, function(d) {
+        return d.key;
+      }).call(function(d) {
+        d.enter().append("text");
+      }).call(function(d) {
+        d.exit().remove();
+      }).attr("y", function(d, i) {
+        return i + "em";
+      }).attr("x", "1em").text(function(d) {
+        return d.key;
+      })
+
+      li.selectAll("circle").data(items, function(d) {
+        return d.key;
+      }).call(function(d) {
+        d.enter().append("circle");
+      }).call(function(d) {
+        d.exit().remove();
+      }).attr("cy", function(d, i) {
+        return i - 0.25 + "em";
+      }).attr("cx", 0).attr("r", "0.4em").style("fill", function(d) {
+        console.log(d.value.color);
+        return d.value.color;
+      })
+
+      var lbbox = li[0][0].getBBox();
+      lb.attr("x", (lbbox.x - legendPadding)).attr("y",
+          (lbbox.y - legendPadding)).attr("height",
+          (lbbox.height + 2 * legendPadding)).attr("width",
+          (lbbox.width + 2 * legendPadding));
+    })
+    return g;
+  }  
+  
   var brush2 = function() {
     _self.main_x.domain(brush.empty() ? _self.mini_x.domain() : brush.extent());
     for ( var key in _self.main_y) {
